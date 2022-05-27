7205da994245780d95343261db33c909c8c7aace
angel <angelm.moyano@gmail.com>
2022-05-27 00:05:16 -0300

    segundo comit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LogoComponent } from './components/logo/logo.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { RedessocialesComponent } from './components/redessociales/redessociales.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LogoComponent,
+    BannerComponent,
+    RedessocialesComponent
   ],
   imports: [
     BrowserModule,
