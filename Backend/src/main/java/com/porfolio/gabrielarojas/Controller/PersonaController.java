cf9dcab972c516da320509c2e464be842ce8750a
angel <angelm.moyano@gmail.com>
2022-05-28 01:07:50 -0300

    primer commit backend 2

--- Backend/src/main/java/com/porfolio/gabrielarojas/Controller/PersonaController.java
+++ Backend/src/main/java/com/porfolio/gabrielarojas/Controller/PersonaController.java
@@ -1,2 +1,43 @@
-package com.porfolio.gabrielarojas.Controller;public class PersonaController {
+package com.porfolio.gabrielarojas.Controller;
+
+import com.porfolio.gabrielarojas.Service.PersonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import com.porfolio.gabrielarojas.Entity.Persona;
+
+import java.util.List;
+
+@RestController
+public class PersonaController {
+    @Autowired
+    PersonaService personaService;
+
+    @GetMapping("/personas/traer")
+    public List<Persona> getPersona(){
+        return personaService.getPersona();
+    }
+    @PostMapping("/personas/crear")
+    public String createPersona(@RequestBody  Persona persona){
+        personaService.savePersona(persona);
+        return "Persona Creada";
+    }
+    @DeleteMapping("7persona/borrar/{id}")
+    public String deletePersona(@PathVariable Long id){
+        personaService.deletePersona(id);
+        return "La persona fue eliminada";
+    }
+    @PutMapping("/personas/editar/{id}")
+    public Persona editPersona(@PathVariable Long id,
+                              @RequestParam("nombre") String nuevoNombre,
+                              @RequestParam("apellido") String nuevoApellido,
+                              @RequestParam("imagenPortada") String nuevaImagen){
+
+        Persona persona= personaService.findPersona(id);
+        persona.setNombre(nuevoNombre);
+        persona.setApellido(nuevoApellido);
+        persona.setImagenPortada(nuevaImagen);
+        personaService.savePersona(persona);
+        return persona;
+    }
+
 }
--- Backend/src/main/java/com/porfolio/gabrielarojas/Entity/Persona.java
+++ Backend/src/main/java/com/porfolio/gabrielarojas/Entity/Persona.java
@@ -1,2 +1,28 @@
-package com.porfolio.gabrielarojas.Entity;public class Persona {
+package com.porfolio.gabrielarojas.Entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+@Getter
+@Setter
+@Entity
+public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @NotNull
+    @Size(min = 1, max = 50, message = "El campo contiene demasiado caracteres")
+    private String nombre;
+    @NotNull
+    @Size(min = 1, max = 50, message = "El campo contiene demasiado caracteres")
+    private String apellido;
+
+    private String imagenPortada;
+
 }
